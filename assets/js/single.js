de39a58a9383416cfba186d08337ecd522e1caa2
gigi.upNorth <leung.gerry@gmail.com>
2022-07-03 00:17:54 -0400

    [FE] Adding warning for limint of pagination of issues shown

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,7 +1,7 @@
 /** Global Variables
  ******************************************************************************/
 var issueContainerEl = document.querySelector("#issues-container");
-
+var limitWarningEl = document.querySelector("#limit-warning");
 /** Functions
  ******************************************************************************/
 var getRepoIssues = function (repo) {
@@ -13,6 +13,11 @@ var getRepoIssues = function (repo) {
       response.json().then(function (data) {
         // pass response data to dom function
         displayIssues(data);
+
+        //check if api has paginated issues
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
+        }
       });
     } else {
       alert("There was a problem with the request");
@@ -56,6 +61,19 @@ var displayIssues = function (issues) {
     //append issueEl to container
     issueContainerEl.appendChild(issueEl);
   }
+};
+
+var displayWarning = function (repo) {
+  //add text to warning container
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+  var linkEl = document.createElement("a");
+  linkEl.textContent = "See More Issues on GitHub.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
+
+  // append to warning container
+  limitWarningEl.appendChild(linkEl);
 };
 /** Main Program
  ******************************************************************************/
